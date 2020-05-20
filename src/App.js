9f03437dc1279339e50dc230dcf3c4725ef6e141
Aly Hesham <alyhesham.hamza@gmail.com>
2020-05-20 21:39:51 +0200

    second update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import { Navbar, NavbarBrand } from 'reactstrap'
-import Menu from './components/MenuComponent';
+
+
+import Main from './components/MainComponent';
 import './App.css';
 
+
+
+
 class App extends Component {
+
   render() {
     return (
-      <div >
-       <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristornat Con Fusion</NavbarBrand>
-        </div>  
-       </Navbar>
-       <Menu />
-
+      <div className="App">
+        <Main />
       </div>
     );
   }
 }
+
+
 
 export default App;
