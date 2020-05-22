12f6a0eb4af17e8011a834a26a9dfcc8924e8faa
Aly Hesham <alyhesham.hamza@gmail.com>
2020-05-22 02:20:19 +0200

    Header & Footer

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-import { Card, CardImg , CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 
 class DishDetail extends Component {
 
-
-    
 
     renderDish(dish){
         if(dish != null){
@@ -30,12 +28,12 @@ class DishDetail extends Component {
 
     renderComments(dish){
         if(dish != null){
-            
+            let info = {year: 'numeric', month: 'short', day: 'numeric' };
             const comments = this.props.dish.comments.map((comment) => {
                 return(
                     <div key = {comment.id}>
                         <p>{comment.comment}</p>
-                        <p>--{comment.author}, <span>{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</span></p>
+                        <p>--{comment.author}, <span>{new Date(comment.date).toLocaleDateString("en-US", info)}</span></p>
                     </div>
                 );
             });
